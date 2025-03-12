5731c7dbc29369a097af15609aaf41b5ee7c4137
Malak Soni <131973086+22aiml053malak@users.noreply.github.com>
2025-03-12 19:38:35 +0530

    Add files via upload

---
+++ day25.cpp
@@ -0,0 +1,35 @@
+//044
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to find the ceiling of x using
+   binary search */
+int ceilSearch(vector<int>& arr, int x) {
+    int lo = 0, hi = arr.size() - 1, res = -1;
+    
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+  
+        if (arr[mid] < x)
+            lo = mid + 1;      
+        
+        else { // Potential ceiling found
+            res = mid;   
+            hi = mid - 1;
+        }
+    }
+    return res;  
+}
+
+int main() {
+    vector<int> arr = {1, 2, 8, 10, 10, 12, 19};
+    int x = 3;
+    int index = ceilSearch(arr, x);
+    
+    if (index == -1) 
+        cout << "Ceiling of " << x << " doesn't exist in array";
+    else
+        cout << "Ceiling of " << x << " is " << arr[index];
+
+    return 0;
+}
